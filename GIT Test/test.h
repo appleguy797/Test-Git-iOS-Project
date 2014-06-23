bcc8237c7320e2793b991b14190975d9fa5aaf85
operationneenerneener <operationneenerneener@gmail.com>
2014-06-23 00:30:52 -0600

    added a test class

---
+++ GIT Test/test.h
@@ -0,0 +1,13 @@
+//
+//  test.h
+//  GIT Test
+//
+//  Created by Dice's on 6/23/14.
+//  Copyright (c) 2014 InnovaCo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface test : NSObject
+
+@end
---
+++ GIT Test/test.m
@@ -0,0 +1,13 @@
+//
+//  test.m
+//  GIT Test
+//
+//  Created by Dice's on 6/23/14.
+//  Copyright (c) 2014 InnovaCo. All rights reserved.
+//
+
+#import "test.h"
+
+@implementation test
+
+@end
